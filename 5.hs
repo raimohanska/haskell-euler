d31dce6eccf629fc468b95a3febc23dcefe8b113
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-07 08:17:01 +0200

    optimized solution for #5

--- 5.hs
+++ 5.hs
@@ -2,7 +2,19 @@
 
 -- What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-smallest = head $ dropWhile (not . divisible) [20, 40 ..]
-divisible x = all (`factorOf` x) [19, 18, 17, 16, 15, 14, 13, 12, 11]
-factorOf x y = y `mod` x == 0
-          
+smallest = foldr (*) 1 $ combinedFactors [1 .. 20]
+
+factors 1 = []
+factors n | firstFactor == n = [n]
+          | otherwise   = factors firstFactor ++ factors (n `div` firstFactor)
+          where firstFactor = head $ dropWhile (\x -> n `mod` x > 0) [2 ..]   
+                                            
+combineFactors :: [Int] -> [Int] -> [Int]          
+combineFactors [] bs = bs
+combineFactors as [] = as
+combineFactors (a:as) (b:bs) | a == b = a : combineFactors as bs
+                             | a < b  = a : combineFactors as (b:bs)
+                             | a > b  = b : combineFactors (a:as) bs
+                             
+combinedFactors :: [Int] -> [Int]
+combinedFactors as = foldr combineFactors [] (map factors as)
