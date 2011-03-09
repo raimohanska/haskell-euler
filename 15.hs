895e06a589c10a23d1035adc666f8a9d6dca1906
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-09 14:37:53 +0200

    cleanup

--- 15.hs
+++ 15.hs
@@ -1,10 +1,5 @@
 -- How many routes are there through a 20x20 grid?
 
-routes :: Int -> Int -> Int
-routes 0 n = 1
-routes n 0 = 1
-routes n m = (routes (n-1) m) + (routes n (m-1))
-
 -- For dimensions D*D                            
 -- 2D moves total -> (2D)! ways to order the list of moves
 -- the D down moves and the D right moves can each be ordered in D! ways
