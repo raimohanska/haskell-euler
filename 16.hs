efb849a8fc53ceefcdd9f6aecfa158cdfe566b37
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-09 14:38:13 +0200

    easy solution to #16

---
+++ 16.hs
@@ -0,0 +1,6 @@
+-- What is the sum of the digits of the number 2^1000?
+
+digitsOf x | x < 10    = [x]
+           | otherwise = digitsOf (x `div` 10) ++  digitsOf (x `mod` 10)
+
+digitSum exp = sum $ digitsOf $ 2^exp
