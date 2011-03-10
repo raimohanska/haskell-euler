3c526b8d9ff302996aa942a17dd0dbdb6cf756f3
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-10 10:30:35 +0200

    corrected solution for 21

--- 21.hs
+++ 21.hs
@@ -1,19 +1,10 @@
 -- Evaluate the sum of all the amicable numbers under 10000.
 
+import Control.Parallel (par)
+
 sumAmicable n = sum $ filter amicable [1..n]                         
-amicable x = x == (sumProperDivisors $ sumProperDivisors x)
+amicable x = (x /= pair) && (x == (sumProperDivisors $ pair))
+    where pair = sumProperDivisors x
 sumProperDivisors = sum . properDivisors
     where properDivisors x = filter (`divisorOf` x) [1..x `div`2]          
           divisorOf x y = y `mod` x == 0                   
-          
--- TODO: trying to optimize using prime factors etc          
-          
-primeFactors x = pf x primes
-     where pf x (p:ps) | p > x = []
-                       | p `factorOf` x = p : (pf (x `div` p) (p:ps))
-                       | otherwise = pf x ps
-     
-primes = 2 : 3 : filter prime [5 ..]
-prime x = factors x == []
-factors x = filter (`factorOf` x) $ takeWhile (\y -> y ^ 2 <= x) primes          
-factorOf x y = y `mod` x == 0      
