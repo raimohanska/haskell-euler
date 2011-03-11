b932938df90cd436b63ee58f7bcf19a58fc35ab5
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-11 13:45:58 +0200

    solved #19

--- 19.hs
+++ 19.hs
@@ -1,12 +1,15 @@
 -- How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 
-data Date = Date { day :: Int, month :: Int, year :: Int }
-    deriving (Show, Eq)
-
+data Date = Date { year :: Int, month :: Int, day :: Int }
+    deriving (Show, Eq, Ord)
+    
+data DayOfWeek = Mon|Tue|Wed|Thu|Fri|Sat|Sun
+    deriving (Show, Eq, Enum, Ord)
+    
 nextDate :: Date -> Date
-nextDate (Date day month year) | day < (daysPerMonth month year) - 1 = Date (day+1) month year
-                               | month < 12                          = Date 1 (month+1) year
-                               | otherwise                           = Date 1 1 (year+1)
+nextDate (Date year month day ) | day < (daysPerMonth month year) - 1 = Date year month (day+1)
+                                | month < 12                          = Date year (month+1) 1
+                                | otherwise                           = Date (year+1) 1 1
 
 daysPerMonth 2 year | (leapYear year) = 29
                     | otherwise       = 28
@@ -19,5 +22,17 @@ leapYear year | year `mod` 4 /= 0   =  False
               | year `mod` 100 == 0 = False
               | otherwise           = True
                         
-                        
+dates = iterate nextDate $ Date 1900 1 1
+datesWithDayOfWeek = zip dates daysOfWeek
+    where 
+        daysOfWeek = map toDayOfWeek [0..]    
+        toDayOfWeek x = toEnum (x `mod` 7) :: DayOfWeek
+        
+matching = length $ filter sundayFirst $ takeWhile (< (Date 2001 1 1, Mon)) $ dropWhile (< (Date 1901 1 1, Mon)) datesWithDayOfWeek
+    where sundayFirst (Date _ _ 1, Sun) = True
+          sundayFirst _                 = False
+
+
+
+
                      
