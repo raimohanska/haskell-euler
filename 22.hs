2998ebc6748ad7a581ceb51bf33cdf19b81ff613
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-11 14:34:48 +0200

    add sorting -> correct solution

--- 22.hs
+++ 22.hs
@@ -1,9 +1,11 @@
 -- What is the total of all the name scores in the file?
 -- For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938  53 = 49714.
 
+import Data.List
+
 main = do
     fileContent <- readFile "names.txt"    
-    putStrLn $ show $ nameListScore $ parseNames fileContent
+    putStrLn $ show $ nameListScore $ sort $ parseNames fileContent
     return ()
     
 parseNames = splitBy ',' . filter (/= '"')
