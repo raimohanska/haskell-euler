a4485e7199a33ae91069f88194fe1fd528d700d1
Juha Paananen <juha.paananen@reaktor.fi>
2011-10-24 15:05:10 +0300

    correct solution for 34

--- 34.hs
+++ 34.hs
@@ -2,10 +2,9 @@
 -- Find the sum of all numbers which are equal to the sum of the factorial of their digits.
 -- Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 
-sumOfThose = sum $ filter equalToFactorialSum $ [10 .. 10^upperLimitForDigits]
-
-upperLimitForDigits = head $ filter (impossible) $ [2..]
-  where impossible n = 10 ^ (n-1) > n * (factorial 9)
+sumOfThose = sum $ filter equalToFactorialSum $ [3 .. 10^maxDigits]
+  where maxDigits = last $ takeWhile (possibleDigits) $ [2..]
+        possibleDigits n = 10^(n-1) - 1 <= (n-1) * (factorial 9)
 
 equalToFactorialSum n = n == (sum $ map factorial $ digitsOf n)
 
